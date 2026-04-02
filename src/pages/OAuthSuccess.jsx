30aa3268b020a773605555332bcb23ed894d7af5
Aakash Singh <aakash.singh@w3villa>
2026-04-02 22:15:40 +0530

    fina

--- src/pages/OAuthSuccess.jsx
+++ src/pages/OAuthSuccess.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import axios from "axios";
 
 const OAuthSuccess = () => {
